132fef5878e4ef594858407f7a25e90377196530
BlueTot <bluetotkc@gmail.com>
2025-06-27 16:25:39 +0100

    working on improving inheritance and polymorphism

--- inheritance_and_polymorphism/c02.py
+++ inheritance_and_polymorphism/c02.py
@@ -1,54 +1,87 @@
-class Vehicle:
-    def __init__(self, make: str, model: str, year: int) -> None:
-        self.make: str = _____
-        self.____: str = model
-        self.year: int = ______
-
-    def description(self) -> str:
-        return f"{self.year} {self.make} {self.model}"
+from math import pi
 
 
-class Car(________):
-    def __init__(self, make: str, model: str, year: int, fuel_type: str) -> None:
-        _________.__init__(make, model, year)
-        self.fuel_type: str = fuel_type
+class Rectangle:
+    """
+        Class for a rectangle
+        A rectangle is minimally defined using its length and width
+    """
+    def __init__(self, length: float, width: float) -> None:
+        self.__length = length
+        self.__width = width
 
-    def description(self) -> str:  # add fuel type to the parent class's description
-        return super()._________ + f", Fuel Type: {self.fuel_type}"
+    '''GETTERS'''
+
+    @property
+    def length(self) -> float:
+        return self.__length
+
+    @property
+    def width(self) -> float:
+        return self.__width
+
+    '''AREA AND PERIMETER'''
+
+    def area(self) -> float:
+        return self.__length * self.__width
+
+    def perimeter(self) -> float:
+        return 2 * (self.__length + self.__width)
+
+    '''SPECIAL METHODS'''
+
+    # string representation method
+    def __repr__(self) -> str:
+        return f"Rectangle(length={self.__length}, width={self.__width})"
+
+    # equality operator method
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Rectangle) and  \
+                sorted([self.__length, self.__width]) == sorted([other.length, other.width])
 
 
-class ElectricCar(________):
-    def __init__(self, make: str, model: str, year: str, battery_capacity: int) -> None:
-        __________.__init__(make, model, year, fuel_type="Electric")
-        self._______________ = battery_capacity
+class Circle:
+    """
+        Class for a circle
+        A circle is minimally defined using its radius
+    """
+    def __init__(self, radius: float) -> None:
+        self.__radius = radius
 
-    def description(self) -> str:  # add battery capacity to parent class's description
-        return ________.description() + f", Battery Capacity: {self.battery_capacity} kWh"
+    '''GETTERS'''
+
+    @property
+    def radius(self) -> float:
+        return self.__radius
+
+    '''AREA AND PERIMETER'''
+
+    def area(self) -> float:
+        return pi * self.__radius * self.__radius
+
+    def perimeter(self) -> float:
+        return 2 * pi * self.__radius
+
+    '''SPECIAL METHODS'''
+
+    # string representation method
+    def __repr__(self) -> str:
+        return f"Circle(radius={self.__radius}"
+
+    # equality operator method
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Circle) and self.__radius == other.radius
 
 
-class Bicycle(_________):
-    def __init__(self, make: str, model: str, year: int, gear_count: int) -> None:
-        super().__init__(make, model, year)
-        self.gear_count: int = __________
+# using polymorphism to get the area and perimeter of each shape
+shapes : list[Rectangle | Circle] = [Rectangle(4, 5), Circle(3)]
+for shape in shapes:
+    print(shape, shape.area(), shape.perimeter())
 
-    def description(self) -> str:
-        return super().description() + f", Gear Count: {self.gear_count}"
-
-    def ring_bell(self) -> str:  # unique method of bicycle
-        return "Ring ring!"
-
-
-# Create instances of each class
-vehicle: Vehicle = Vehicle("GenericMake", "GenericModel", 2020)
-car: Car = Car("Toyota", "Corolla", 2021, "Gasoline")
-electric_car: ElectricCar = ElectricCar("Tesla", "Model 3", 2022, 75)
-bicycle: Bicycle = Bicycle("Giant", "Escape 3", 2023, 21)
-
-# Basic description of each vehicle
-print(vehicle.description())
-print(car.description())
-print(electric_car.description())
-print(bicycle.description())
-
-# Test unique method of Bicycle
-print(bicycle._________())
+# tests for the equality operator
+print(Rectangle(3, 4) == Rectangle(3, 4))
+print(Rectangle(3, 4) == Rectangle(4, 3))
+print(Rectangle(3, 4) == Rectangle(3, 5))
+print(Circle(3) == Rectangle(3, 4))
+print(Circle(4) == Circle(4))
+print(Circle(3) == Circle(5))
