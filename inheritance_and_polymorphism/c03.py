3d6dd29bbd6cdaa25d293575deb672dee5c08150
BlueTot <bluetotkc@gmail.com>
2025-06-28 09:24:59 +0100

    rewrote c03 of inheritance and polymorphism

--- inheritance_and_polymorphism/c03.py
+++ inheritance_and_polymorphism/c03.py
@@ -1,51 +1,53 @@
-# dog class
-class Dog:
+class Animal:
+    """
+        Superclass for all the animals
+    """
+    def __init__(self, name: str) -> None:
+        self._name = name
 
-    def __init__(self) -> None:
+    @property
+    def name(self) -> str:
+        return self._name
+
+    # for us to use the Animal type annotation for all the subclasses, we must define the speak() method
+    # this is a virtual method - it can be overrided later by subclasses
+    def speak(self) -> None:
         pass
 
-    def make_sound(self) -> None:
+
+class Dog(Animal):
+    """
+        Dog has a speak() method
+    """
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def speak(self) -> None:
         print("Woof!")
 
+class Cat(Animal):
+    """
+        Cat has a speak() method
+    """
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+    
+    def speak(self) -> None:
+        print("Meow")
 
-# cat class
-class Cat:
+class Horse(Animal):
+    """
+        Horse has a speak() method
+    """
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
 
-    def __init__(self) -> None:
-        pass
+    def speak(self) -> None:
+        print("Neigh")
 
-    def make_sound(self) -> None:
-        print("Meow!")
+# this will work because Dog, Cat, horse all have a speak() method
+# this is an example of polymorphism
+animals : list[Animal] = [Dog("dog"), Cat("cat"), Horse("horse")]
+for animal in animals:
+    animal.speak()
 
-
-# bulldog class
-class Bulldog(Dog):
-    '''Bulldog class, inherits from Dog'''
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    def make_sound(self) -> None:  # OVERRIDING: Override method inherited from Dog
-        print("Woof! I'm a Bulldog!")
-
-
-# persian cat class
-class PersianCat(Cat):
-    '''Persian Cat class, inherits from Cat'''
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    def make_sound(self) -> None:  # OVERRIDING: Override method inherited from Cat
-        print("Meow! I'm a Persian Cat!")
-
-
-# Create instances of each class and add them to a list
-# NOTE - the type annotation means the list contains objects that are either a Dog or Cat
-#        Inheritance allows us to assign the parent class' type to an instance of a child class
-my_pets: list[Dog | Cat] = [Dog(), Cat(), Bulldog(), PersianCat()]
-
-# RUN-TIME POLYMORPHISM: one method interface can be called for objects of different classes
-# This is type safe because both Dog and Cat have the make_sound() method.
-for pet in my_pets:
-    pet.make_sound()
